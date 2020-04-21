ac666e79b335cce86d247920e712bb4de502a727
cs0uth <saeidaliei2019@gmail.com>
2020-04-21 13:23:39 +0200

    add fucntion declarations

--- DataStructure/HashTable/hash_table.h
+++ DataStructure/HashTable/hash_table.h
@@ -11,3 +11,8 @@ typedef struct
     int count;
     item** items;//an array of pointers, each elem is an item
 } table;
+
+static item* new_item(const char* k, const char* v);
+static void del_item(item* i);
+table* new_table();
+void del_table(table* t);
