733d9b95bb412a953f37a533285bbf7f9afd8cae
Saeid Aliei <saeidaliei2019@gmail.com>
2021-11-06 15:27:02 +0100

    test non-zero elements only

--- Python/ICTP-SciDev2021/test.py
+++ Python/ICTP-SciDev2021/test.py
@@ -1,8 +1,12 @@
 from utils.path import get_children
 from utils.land import load_data
 import dask.array as ds
+import numpy.ma as ma
 import numpy as np
 
+# TODO: maybe work with sparse matrice using scipy
+# https://docs.scipy.org/doc/scipy/reference/sparse.html
+# TODO: just store non zero elements
 
 # data = np.array([[1, 1, 0, 0, 1, 1, 1, 1, 0, 1],
                  # [0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
@@ -13,7 +17,7 @@ import numpy as np
                  # [1, 0, 0, 0, 1, 1, 0, 1, 1, 1],
                  # [1, 0, 0, 0, 1, 1, 0, 1, 1, 1],
                  # [1, 1, 0, 0, 1, 1, 1, 1, 0, 1],
-                 # [1, 1, 0, 0, 0, 1, 1, 1, 0, 0]])
+                 # [1, 1, 0, 0, 0, 1, 1, 1, 0, 0]], dtype=bool)
 
 # data = np.array([[1, 1, 0, 0, 1, 1, 1, 1, 0, 1],
                  # [0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
@@ -22,11 +26,12 @@ import numpy as np
                  # [1, 1, 0, 0, 0, 1, 1, 1, 0, 0]])
 
 # data = np.array([[1, 0, 1],
-                # [0, 1, 1]])
-
+                # [0, 1, 1]], dtype=bool)
+# data = ma.masked_where(data == 0, data)
 filename = "gl-latlong-1km-landcover.bsq"
 data = load_data(filename)
-data = ds.from_array(data)
+data = data.astype(dtype=bool)
+# data = ds.from_array(data)
 data = data[:1000, :1000]
 
 shape = data.shape
@@ -37,17 +42,18 @@ N, M = shape
 # this is equal to data
 # data[row, col]
 
-# indices = np.arange(N * M)
-indices = ds.arange(N * M)
-adjacency = ds.zeros((N*M, N*M), dtype='int')
+indices = np.arange(N * M)
+# indices = ds.arange(N * M)
+# adjacency = ds.zeros((N*M, N*M), dtype='int')
+# adjacency = np.zeros((N*M, N*M), dtype='int')
 
-for index in indices:
-    node = np.unravel_index(index, shape)
-    # print(node)
-    children = get_children(data, node)
-    raveled_children = [np.ravel_multi_index(child, data.shape) for child in children]
-    # print(raveled_children)
-    for i in raveled_children:
+# for index in indices:
+    # node = np.unravel_index(index, shape)
+    # # print(node)
+    # children = get_children(data, node)
+    # raveled_children = [np.ravel_multi_index(child, data.shape) for child in children]
+    # # print(raveled_children)
+    # for i in raveled_children:
         adjacency[index, i] = 1
     # print(children)
 
