f0422413bc7d81b869592919fc5b148dbc0d2eda
Saeid Aliei <saeidaliei2019@gmail.com>
2021-11-09 10:36:51 +0100

    clean the source code

--- Python/ICTP-SciDev2021/utils/path.py
+++ Python/ICTP-SciDev2021/utils/path.py
@@ -1,78 +1,20 @@
-#!/usr/bin/env python
 """Module file for exercise 1 for the ICTP SciDev2021 workshop.
 For the problem description refere to:
 https://corbetta.phys.tue.nl/pages/ictp-l21-121.html
 
 TODO:
     * Fix long search time for large grids.
+        * Smart search, only search a square grid containing the target and source nodes.
+        * Coarsen the grid.
+    * Test dask arrays for parallel and distributed search.
+    * Expand to find a path if the source and target are in water.
+    * Use a weighted graph, depending on the landscape with an appropriate weight.
 
 """
-
 from utils.land import load_data, get_index
 from typing import Union, Tuple, Deque, List
 from collections import deque
-import dask.array as ds
 import numpy as np
-#from dask.distributed import Client
-#from dask.diagnostics import ProgressBar
-
-# from utils.land import ...
-# from utils.path import dfs, ...
-
-# unit tests
-
-# TODO: make path unique
-
-# https://www.interviewbit.com/tutorial/depth-first-search/
-# Since we are using a list as opposed to a set in Python to keep track of visited vertices, the search to see if a vertex has already been visited has a linear runtime as opposed to constant runtime
-# print all the nodes for the path in a constant time, should consider the size of the path
-# store raveled indices instead of a tuple maybe in a set for large grids
-# neighbors instead of children
-# https://www.baeldung.com/cs/iterative-deepening-vs-depth-first-search
-# test ravel and unravel for speed
-# test stack limit, ulimit -s unlimited
-# distributed and parallel version with dask, client, ... to benchmark...
-
-# TODO: see stack and deque and queue efficiencies?
-# TODO: validity and goal checking in a separate function
-# DFS, IDDLS, BFS, Dijkstra?
-# BFS in a graph with no weight and direction is the same as Dijkstra(weight=1, one direction)
-# DFS doesn't find the shortest path necessarily!! simple test: start=(0, 6), end=(2, 5)
-# https://stackoverflow.com/questions/14784753/shortest-path-dfs-bfs-or-both
-# TODO: guess the max_depth depending on the distance between points to optimize it a bit.
-# strange cases: start=(0, 0), end=(0, 2)
-# make checking for validity simple, by using hard cases for edges , etc.
-# https://eddmann.com/posts/using-iterative-deepening-depth-first-search-in-python/
-# https://www.baeldung.com/cs/iterative-deepening-vs-depth-first-search
-
-# TODO: find a way to work with large grids!
-# TODO: maybe at end we have to coarsen the map!
-# if you can't find a path between two land points you have to fly!
-# TODO: make a graph out of the grid to prun a lot of unneeded nodes with probably adjancy matrix!
-# TODO: a class that makes a graph out of a map, if weighted takes the different landscape type into account!
-# TODO: if point is on water, children should be only grid points where it is 0, so go by boat, walk, fly, swim
-
-# algorithm name
-# ITERATIVE DEEPENING DEPTH LIMITED DEPTH FIRST SEARCH
-
-# in a satellite navigation system this will cost us, and it's not
-# a very smart way of searching paths
-
-
-#originmap[x+1,y]=np.ravel_multi_index([x,y], (max_val,max_val))
-
-# client = Client(n_workers=4, processes=False, threads_per_worker=1)
-
-
-# for this case np is much faster than ds
-# arr = ds.from_array(np_arr)
-
-
-# mapfile = "gl-latlong-1km-landcover.bsq"
-# mapdata = load_data(mapfile, lazyload=False)
-# mapdata = mapdata[:1000, :2000]
-# mapdata[mapdata > 0] = 1
-
 
 
 def is_target(node: Tuple[int, int], target: Tuple[int, int]) -> bool:
