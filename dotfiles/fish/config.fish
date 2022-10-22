624033b8b7914abf740764c137dbfe1f362c730b
Saeid Aliei <saeidaliei2019@gmail.com>
2022-10-22 18:52:57 +0330

    update fish config

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -2,6 +2,8 @@
  set -gx VISUAL (type -p vim)
  set -gx BROWSER (type -p google-chrome-stable)
 
+fish_add_path -p ~/.local/bin
+
 # Redirect users shell to fish when starting with su
 function su
     command su --shell=/usr/bin/fish $argv
@@ -29,5 +31,16 @@ end
 
 function penv
     source $HOME/Documents/penvs/$argv/bin/activate.fish
+end
+
+function lh
+    ls -lash $argv
+end
+
+function ssh_add
+    if ! pgrep ssh-agent > /dev/null
+        eval (ssh-agent -c)
+    end
+    ssh-add ~/.ssh/id_rsa_github_manjaro_i3
 end
 
