2529496ba1263a4004b2a37be377ab21a65c308e
Saeid Aliei <saeidaliei2019@gmail.com>
2022-11-19 00:35:59 +0330

    debug print rectangle struct

--- Rust/official-rust-by-example/03-1-structures.rs
+++ Rust/official-rust-by-example/03-1-structures.rs
@@ -24,6 +24,7 @@ struct Unit;
 struct Pair(i32, f32);
 
 // a struct with two fields
+#[derive(Debug)]
 struct Point {
     x: f32,
     y: f32,
@@ -40,7 +41,7 @@ fn main() {
     let name = String::from("Saeid");
     let age = 28;
     // create struct with field init shorthand
-    let saeid = Person { name, age };
+    let saeid = Person {name, age};
 
     // print debug struct using :?, Debug trait is already implemented for many types,
     // to debug print a container, the items inside the container must also implement Debug trait.
@@ -48,10 +49,31 @@ fn main() {
     // pretty print
     println!("{:#?}", saeid);
 
-    let point: Point = Point { x: 1.23, y: 4.56 };
+    let point: Point = Point {x: 1.23, y: 4.56};
     println!("point coords: ({}, {})", point.x, point.y);
     
     // using struct update syntax to make use of coords of the other point struct
-    let bottom_right = Point { x: 7.89, ..point };
+    let bottom_right = Point {x: 7.89, ..point};
     println!("bottom_right coords: ({}, {})", bottom_right.x, bottom_right.y);
+
+    // destructure(?) the point using `let` binding
+    let Point {x: left_edge, y: top_edge} = point;
+
+    let rect = Rectangle {
+        top_left: Point {x: left_edge, y: top_edge},
+        bottom_right: bottom_right,
+    };
+    // error: 'Point' cannot be formatted with the default formatter, doesn't implement std::fmt::Display
+   // println!("rect coords: ({}, {})", rect.top_left, rect.bottom_right);
+   // print with debug formatter
+   println!("rect coords: ({:?}, {:?})", rect.top_left, rect.bottom_right);
+  
+    let _unit = Unit;
+
+    let pair = Pair(1, 3.14);
+    println!("Tuple struct: ({}, {})", pair.0, pair.1);
+    
+    // destructure(?) a tuple struct
+    let Pair(integer, decimal) = pair;
+    println!("Pair struct: ({:?}, {:?})", integer, decimal);
 }
