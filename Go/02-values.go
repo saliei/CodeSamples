12dcccb90fc32f328b7512f0a2caf3c3a6a94200
saliei <saeid.aliei@gmail.com>
2023-07-10 17:08:40 +0200

    add init examples of go

---
+++ Go/02-values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("string" + " plused")
+	fmt.Println(1 + 3.0)
+	fmt.Println(1 + 2)
+	fmt.Println(1 / 2.0)
+	fmt.Println(4 / 2)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
